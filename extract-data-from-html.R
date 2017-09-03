4cecda99f7529770521dfd65c8776a5faf2151e3
Boyan Bejanov <bbejanov@gmail.com>
2017-09-03 17:56:02 -0400

    got a dataframe with abstracts and metadata

---
+++ extract-data-from-html.R
@@ -0,0 +1,54 @@
+#!/usr/bin/R --no-save
+
+library(XML)
+
+parse_row <- function(fname) {
+    doc <- htmlParse(fname)
+
+    ret <- list()
+
+    foo <- xpathApply(doc, "//h1[@class='post-heading']", xmlValue)
+    if (length(foo) != 1) stop("unable to parse title in ", fname)
+    ret[["title"]] <- foo[[1]] 
+
+    foo <- xpathApply(doc, "//div[@class='post-date']", xmlValue)
+    if (length(foo) != 1) stop("unable to parse date in ", fname)
+    ret[["date"]] <- foo[[1]] 
+
+    foo <- xpathApply(doc, "//div[@class='post-authors']/a", xmlValue)
+    if (length(foo) < 1) stop("unable to parse authors in ", fname)
+    ret[["authors"]] <- do.call(paste, c(sep=",", foo)) 
+
+    foo <- xpathApply(doc, "//div[@class='topic taxonomy']/a", xmlValue)
+    if (length(foo) < 1) stop("unable to parse topics in ", fname)
+    ret[["topics"]] <- do.call(paste, c(sep=",", foo)) 
+
+    foo <- xpathApply(doc, "//div[@class='jel taxonomy']/a", xmlValue)
+    if (length(foo) < 1) {
+        ret[["jel"]] <- ""
+    } else {
+        ret[["jel"]] <- do.call(paste, c(sep=",", foo)) 
+    }
+
+    foo <- xpathApply(doc, "//span[@class='post-content']", xmlValue)
+    if (length(foo) != 1) stop("unable to parse abstract in ", fname)
+    ret[["abstract"]] <- trimws(strsplit(trimws(foo[[1]]), "\\n|\\r")[[1]][[1]]) 
+
+    return( ret )
+}
+
+for (kind in c("swp", "san", "sdp")) {
+    cat("Parsing", kind, "...")
+    data <- data.frame()
+    for (year in list.files("data") ) {
+        dirname <- file.path("data", year, kind)
+        for( fn in list.files(dirname) ) {
+            row <- parse_row(file.path(dirname, fn))
+            data <- rbind(data, as.data.frame(row, stringsAsFactors=FALSE))
+        }
+    }
+    cat("saving", nrow(data), "records\n")
+    write.csv(data, file=file.path("data", paste(kind, "csv", sep=".")), row.names=FALSE)
+}
+
+
