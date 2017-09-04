a365c97d5828f49e68d23f267523ad0b365659ab
Boyan Bejanov <bbejanov@gmail.com>
2017-09-04 13:59:44 -0400

    added some wordcloud and comparison plots

--- scratch.r
+++ scratch.r
@@ -14,3 +14,40 @@ data$month <- factor(data$month, levels=month.name)
 ddply(data, c("year", "month"), summarize, count=length(year))
 
 
+##
+## Part 2: 
+
+library(tm)
+library(wordcloud)
+
+make_corp <- function(x) {
+    corp <- SimpleCorpus(VectorSource(x))
+    corp <- tm_map(corp, tolower)
+    corp <- tm_map(corp, removePunctuation)
+    corp <- tm_map(corp, function(x) removeWords(x, stopwords()))
+    return(corp)
+}
+corp <- make_corp(data$title)
+wordcloud(corp[ data$year==2010 ], max.words=50)
+
+##
+# compare two years
+
+byYear = list()
+for (y in unique(data$year)) {
+    byYear <- c(byYear, paste(data$abstract[ data$year==y ], collapse=" "))
+}
+
+corp.byYear <- SimpleCorpus(VectorSource(byYear))
+corp.byYear <- tm_map(corp.byYear, tolower)
+corp.byYear <- tm_map(corp.byYear, removePunctuation)
+corp.byYear <- tm_map(corp.byYear, function(x) removeWords(x, stopwords()))
+
+tdm <- TermDocumentMatrix(corp.byYear)
+tdm <- as.matrix(tdm)  
+colnames(tdm) <- c(as.character(2010:2017))
+comparison.cloud(tdm[,3:4], max.words=50, random.order=FALSE)
+
+wordcloud(corp.byYear, max.words=100, random.order=FALSE)
+
+
